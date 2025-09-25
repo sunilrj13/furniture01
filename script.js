383eed05f5f11afa50bad6e57c879a16db098ffe
Sunil Joshi <sunil5lnp@gmail.com>
2025-09-25 09:48:17 +0530

    try First

---
+++ script.js
@@ -0,0 +1,22 @@
+<script>
+  // require bootstrap.bundle.js loaded before this
+  (function(){
+    var mobileMenu = document.getElementById('mobile-menu');
+    if(!mobileMenu) return;
+    var bsCollapse = null;
+
+    // lazy-get instance
+    function getCollapse() {
+      bsCollapse = bsCollapse || bootstrap.Collapse.getOrCreateInstance(mobileMenu);
+      return bsCollapse;
+    }
+
+    document.querySelectorAll('#mobile-menu .nav-link').forEach(function(link){
+      link.addEventListener('click', function(){
+        // hide collapse on link click (mobile)
+        var c = getCollapse();
+        c.hide();
+      });
+    });
+  })();
+</script>
