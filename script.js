46f3843843fe9db201dcb81b4a0b71414bad15e7
Sunil Joshi <sunil5lnp@gmail.com>
2025-09-26 01:47:59 +0530

    just A minor Change

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-<script>
-  // require bootstrap.bundle.js loaded before this
-  (function(){
-    var mobileMenu = document.getElementById('mobile-menu');
-    if(!mobileMenu) return;
-    var bsCollapse = null;
+// require bootstrap.bundle.js loaded before this
+(function(){
+  let mobileMenu = document.getElementById('mobile-menu');
+  if(!mobileMenu) return;
+  let bsCollapse = null;
 
-    // lazy-get instance
-    function getCollapse() {
-      bsCollapse = bsCollapse || bootstrap.Collapse.getOrCreateInstance(mobileMenu);
-      return bsCollapse;
-    }
+  // lazy-get instance
+  function getCollapse() {
+    bsCollapse = bsCollapse || bootstrap.Collapse.getOrCreateInstance(mobileMenu);
+    return bsCollapse;
+  }
 
-    document.querySelectorAll('#mobile-menu .nav-link').forEach(function(link){
-      link.addEventListener('click', function(){
-        // hide collapse on link click (mobile)
-        var c = getCollapse();
-        c.hide();
-      });
+  document.querySelectorAll('#mobile-menu .nav-link').forEach(function(link){
+    link.addEventListener('click', function(){
+      // hide collapse on link click (mobile)
+      var c = getCollapse();
+      c.hide();
     });
-  })();
-</script>
+  });
+})();
